decd4c2161e8449b0586ca6b081cab68fedb5574
VIKAS MISHRA <149103708+vikasmishra1711@users.noreply.github.com>
2026-05-03 20:24:46 +0530

    Comment out tests and add print statement
    Comment out test functions and add a print statement.

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,9 +1,10 @@
 from src.math_operation import add,sub
 
-def test_add():
-    assert add(2,3)==5
-    assert add(3,3)==6
-    
-def test_sub():
-    assert sub(5,2)==3
-    assert sub(7,3)==4
+# def test_add():
+#     assert add(2,3)==5
+#     assert add(3,3)==6
+
+print(add(5+6))
+# def test_sub():
+#     assert sub(5,2)==3
+#     assert sub(7,3)==4
